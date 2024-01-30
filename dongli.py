f82af5b8ed36809a08379f092034e30fd8a20ae5
Dongli99 <dongliliu0@gmail.com>
2024-01-30 09:29:25 -0800

    set up root

--- dongli.py
+++ dongli.py
@@ -1,4 +1,15 @@
 from tkinter import *
 from tkinter import messagebox
 
+# Declare and set root from tkinter
 root = Tk()
+root.geometry('600x400')
+root.title('Centennial College')
+root.config(background='lightgreen')
+
+# Create elements on the page
+# header
+header = Label(root, text='ICET Student Survey', )
+
+# Show the window
+root.mainloop()
